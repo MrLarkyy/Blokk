35a9fdfe5664862e1e832eabac5012479d8b5355
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-12 23:16:33 +0100

    Remove unused Kotlin import statements

--- src/main/kotlin/gg/aquatic/blokk/Blokk.kt
+++ src/main/kotlin/gg/aquatic/blokk/Blokk.kt
@@ -5,9 +5,6 @@ import gg.aquatic.kregistry.MutableRegistry
 import gg.aquatic.kregistry.Registry
 import org.bukkit.Location
 import org.bukkit.block.data.BlockData
-import kotlin.collections.component1
-import kotlin.collections.component2
-import kotlin.collections.forEach
 
 abstract class Blokk {
 
